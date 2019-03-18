64f6caa3156eef9f797019f81a07da3ee8b5a599
luanhongjie <506670809@qq.com>
2019-03-18 16:37:00 +0800

    添加新字段

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -22,5 +22,7 @@ namespace MvcMovie.Models
         [Display(Name ="价格")]
         [Column(TypeName ="decimal(18,2)")]
         public decimal Price { get; set; }
+        [Display(Name ="收视率")]
+        public string Rating { get; set; }
     }
 }
