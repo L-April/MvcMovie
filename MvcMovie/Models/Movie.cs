f70627f13239ba411a4852f0f8851fb18e34c0a2
luanhongjie <506670809@qq.com>
2019-03-18 16:48:07 +0800

    添加验证

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -10,19 +10,33 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
-        [Display(Name ="标题")]
+
+        [Display(Name = "标题")]
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Title { get; set; }
 
-        
         [DataType(DataType.Date)]
         [Display(Name = "发布日期")]
+        //[DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",ApplyFormatInEditMode =true)] //(建议使用DataType)
         public DateTime ReleaseDate { get; set; }
-        [Display(Name ="流派")]
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [StringLength(30)]
+        [Required]
+        [Display(Name = "流派")]
         public string Genre { get; set; }
-        [Display(Name ="价格")]
-        [Column(TypeName ="decimal(18,2)")]
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Display(Name = "价格")]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
-        [Display(Name ="收视率")]
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Display(Name = "收视率")]
+        [Required]
         public string Rating { get; set; }
     }
 }
